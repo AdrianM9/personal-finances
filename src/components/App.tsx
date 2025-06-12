31ecae06bfa7ce7dd540bfd693085a38ad744fe7
Adrian Mirea <amirea99@gmail.com>
2025-06-13 01:05:22 +0300

    Install Framework7

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,17 @@
+"use client";
+
+import Framework7 from "framework7";
+import Framework7React, { App as Framework7App, Block, View } from "framework7-react";
+import "framework7/css/bundle";
+
 export default function App() {
+  Framework7.use(Framework7React);
+
   return (
-    <div>Personal Finances</div>
+    <Framework7App>
+      <View main>
+        <Block>Personal Finances</Block>
+      </View>
+    </Framework7App>
   );
 }
